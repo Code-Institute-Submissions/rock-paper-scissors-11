af51f16ee24f98485cb21d467a6db69d82b80bda
SirJarvis <ninolockdown@protonmail.com>
2021-10-19 02:27:21 +0000

    Add button, modal to html. Add style to CSS for button and it's content. Add JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Declare constants for DOM elements
+
 let humanScore = 0;
 let machineScore = 0;
 const humanScorespan = document.getElementById("human-score");
@@ -7,3 +9,93 @@ const resultp = document.querySelector(".result > p");
 const rockdiv = document.getElementById("rock");
 const paperdiv = document.getElementById("paper");
 const scissorsdiv = document.getElementById("scissors");
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Functions that show result (You won, You lost, Keep it draw)
+function getMachineChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win(humanChoice, machineChoice) {
+    humanScore++;
+    humanScorespan.innerHTML = humanScore;
+    machineScorespan.innerHTML = machineScore;
+    resultp.innerHTML =humanChoice + " defeats " + machineChoice + ". You won ";
+}
+
+function lose(humanChoice, machineChoice) {
+    machineScore++;
+    humanScorespan.innerHTML = humanScore;
+    machineScorespan.innerHTML = machineScore;
+    resultp.innerHTML =humanChoice + " defeats " + machineChoice + ". You lost ";
+}
+
+function draw(humanChoice, machineChoice) {
+    humanScorespan.innerHTML = humanScore;
+    machineScorespan.innerHTML = machineScore;
+    resultp.innerHTML =humanChoice + " equals " + machineChoice + ". Keep it draw ";
+}
+
+// Function that show in what case will player win, lose or draw
+function game(humanChoice) {
+    const machineChoice = getMachineChoice();
+    switch (humanChoice + machineChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorpaper":
+        win(humanChoice, machineChoice);
+        break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+        lose(humanChoice, machineChoice);
+        break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorscissors":
+        draw(humanChoice, machineChoice);
+        break;
+   }
+}
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+function main() {
+    rockdiv.addEventListener('click', function() {
+        game("rock");
+    });
+    
+    paperdiv.addEventListener('click', function() {
+        game("paper");
+    });
+    
+    scissorsdiv.addEventListener('click', function() {
+        game("scissors");
+    });
+}
+    
+main();
